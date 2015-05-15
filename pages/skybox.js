4489f35a135caf76a7d68e7c93fe005f46241277
Mikhail <blackmika@gmail.com>
2015-05-15 20:21:26 +0300

    skybox layers update

--- pages/skybox.js
+++ pages/skybox.js
@@ -482,12 +482,17 @@ function renderSkybox() {
 	//layered_skybox_mesh[2].material.opacity = transition_3;
 
 
-	var transition_1 = smoothstep( 0, 4, distLY );  // 0 -> 1
-	transition_1 = scale( transition_1, 0.0, 1.0, 0.8, 1.0 );
-	layered_skybox_mesh[0].material.opacity = transition_1 * ( 1 - smoothstep( 35, 60, distLY ) );
+//	var transition_1 = smoothstep( 0, 4, distLY );  // 0 -> 1
+	//transition_1 = scale( transition_1, 0.0, 1.0, 0.8, 1.0 );
+	//layered_skybox_mesh[0].material.opacity = transition_1 * ( 1 - smoothstep( 35, 60, distLY ) );
 
-	var transition_2 = smoothstep( 35, 60, distLY ); // 0 -> 1
+
+	var transition_2 = smoothstep( 1, 35, distLY ); // 0 -> 1
+
+	layered_skybox_mesh[0].material.opacity = 1 - transition_2;
 	layered_skybox_mesh[1].material.opacity = transition_2;
+
+
 
 
 	//layers rotation
